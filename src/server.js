485a0e49671377bc884fb6c2e51cb475a0d198ab
Ana Freitas <ana.freitaas16@gmail.com>
2022-10-15 20:23:56 -0300

    cors required

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 // express 
 const express = require('express');
+const routes = require('./routes');
+const cors = require('cors')
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+
 // port 3000 
-app.listen(3000, () => console.log("Server is running"));
+app.listen(80, () => console.log("Server is running"));
