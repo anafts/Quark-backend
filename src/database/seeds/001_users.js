71aebd5c5d27bb460f3a872d5af6f232e6b6e5c7
Ana Freitas <ana.freitaas16@gmail.com>
2022-10-15 08:31:59 -0300

    users seeds

--- src/database/seeds/001_users.js
+++ src/database/seeds/001_users.js
@@ -3,7 +3,8 @@
  * @returns { Promise<void> } 
  */
 exports.seed = async function(knex) {
-  // Deletes ALL existing entries
+  
+  // users login
   await knex('users').del()
   await knex('users').insert([
     {email: 'professor@example.com', password: 'quark12345'}
