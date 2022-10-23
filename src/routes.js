cbaad7da6eb6a814959ab08f6da4b16cfb85b160
Ana Freitas <ana.freitaas16@gmail.com>
2022-10-23 17:33:47 -0300

    skills routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const UserController = require('./controllers/UserController');
+const SkillController = require('./controllers/SkillController');
 
 const routes = express.Router();
 
-
+// login and signup routes
 routes.post('/login', UserController.login );
 routes.post('/signup', UserController.signup );
+
+// skills routes
+routes.get('/skills', SkillController.listSkill );
+routes.put('/updateSkill/:id', SkillController.updateSkill );
+routes.post('/createSkill', SkillController.createSkill );
+
+
 
 
 
