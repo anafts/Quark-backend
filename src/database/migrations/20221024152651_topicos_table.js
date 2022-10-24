d474283b532cda184c4a74e75b6889c61707c0ea
Ana Freitas <ana.freitaas16@gmail.com>
2022-10-24 18:08:39 -0300

    topics table

---
+++ src/database/migrations/20221024152651_topicos_table.js
@@ -0,0 +1,37 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  
+    return knex.schema.createTable('topics', function (table) {
+        
+        table.increments('id').primary()
+        table.string('title').unique().notNullable()
+        table.string('order').notNullable()
+
+        table.string('active_icon', 1000).notNullable()
+        table.string('disabled_icon', 1000).notNullable()
+
+        table.timestamp('created_at').defaultTo(knex.fn.now())
+        table.timestamp('updated_at').defaultTo(knex.fn.now())
+
+        table
+        .integer('skills_id')
+        .notNullable()
+        .references('id')
+        .inTable('skills')
+        .onDelete('CASCADE')
+      });
+
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+    return knex.schema.dropTable('topics') ;
+
+};
